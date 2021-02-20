31d2923d09ee1c67c4afca31e922bc2d2a9aa362
aakash2109 <shukla.aakash567@gmail.com>
2021-02-20 14:03:01 +0530

    first

---
+++ build/precache-manifest.600be92ea0877d55e7e043209eeb481c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0ec1f380061c4f3f33a70ca138f23d",
+    "url": "/covid19visuals/index.html"
+  },
+  {
+    "revision": "2e99c116fe98cb2f8e47",
+    "url": "/covid19visuals/static/css/main.dc692691.chunk.css"
+  },
+  {
+    "revision": "533eb23f00dbef2c23ca",
+    "url": "/covid19visuals/static/js/2.2f8be43b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19visuals/static/js/2.2f8be43b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e99c116fe98cb2f8e47",
+    "url": "/covid19visuals/static/js/main.cf81b99e.chunk.js"
+  },
+  {
+    "revision": "f661e0d415b66ec401b1",
+    "url": "/covid19visuals/static/js/runtime-main.9e5aafa3.js"
+  },
+  {
+    "revision": "b755f3cda36a2e67100ed1e48416b34d",
+    "url": "/covid19visuals/static/media/image.b755f3cd.png"
+  }
+]);
